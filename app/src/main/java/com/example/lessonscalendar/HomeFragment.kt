044fa580b479a9ee3a8adbf2ed4095431508b3e9
Yuki Naka <yukinaka2168@au.com>
2024-10-15 10:53:07 +0900

    「HOME画面」から「習い事を開始画面」と「習い事の記録画面」に遷移することに成功。

--- app/src/main/java/com/example/lessonscalendar/HomeFragment.kt
+++ app/src/main/java/com/example/lessonscalendar/HomeFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var segmentedControl: RadioGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        segmentedControl = view.findViewById(R.id.segmentedControl)
+
+        val navController = findNavController()
+        segmentedControl.setOnCheckedChangeListener { _, checkedId ->
+
+            when (checkedId) {
+                R.id.start_lesson -> {
+                    navController.navigate(R.id.action_homeFragment_to_startLessonsFragment)
+                }
+                R.id.record_lesson -> {
+                    navController.navigate(R.id.action_homeFragment_to_recordLessonsFragment)
+                }
+            }
+        }
+        return view
     }
 }
