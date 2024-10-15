b74e76fcb22bc4ab58e132cc5519a945004fe0d2
Yuki Naka <yukinaka2168@au.com>
2024-10-15 11:27:51 +0900

    ViewBindingのimport。「習い事を開始画面」と「習い事の記録画面」からの戻る画面遷移に成功。

--- app/src/main/java/com/example/lessonscalendar/StartLessonsFragment.kt
+++ app/src/main/java/com/example/lessonscalendar/StartLessonsFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.lessonscalendar.databinding.FragmentStartLessonsBinding
 
 class StartLessonsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentStartLessonsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start_lessons, container, false)
+    ): View {
+        _binding = FragmentStartLessonsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        binding.backToFragmentHomeView.setOnClickListener {
+            navController.navigate(R.id.action_startLessonsFragment_to_homeFragment)
+        }
     }
 }
